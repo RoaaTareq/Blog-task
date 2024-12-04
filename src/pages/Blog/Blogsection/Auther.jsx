789be9e05ce280701133b85d4eb2b33e95ae9031
roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-12-04 22:25:13 +0300

    testing

--- src/pages/Blog/Blogsection/Auther.jsx
+++ src/pages/Blog/Blogsection/Auther.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Roaa from '../../../assets/Roaa.jpg'
-import Facebook from '../../../assets/Component1.svg'
-import LinkeIn from '../../../assets/Component2.svg'
-import Tiwitter from '../../../assets/Component3.svg'
-import YouTube from '../../../assets/Component4.svg'
+import Roaa from '../../../assets/Roaa.jpg';
+import Facebook from '../../../assets/Component1.svg';
+import LinkeIn from '../../../assets/Component2.svg';
+import Tiwitter from '../../../assets/Component3.svg';
+import YouTube from '../../../assets/Component4.svg';
 import "../../../styles/author.scss"; // Import SCSS file
 
 function Author() {
@@ -13,30 +13,26 @@ function Author() {
         <h4 className="author__title">ABOUT THE AUTHOR</h4>
         <div className="author__content">
           <div className="author__image-container">
-            <img src={Roaa} alt="Author" className="author__image" />
+            <img src={Roaa} alt="Photo of Roaa Abushaqrah" className="author__image" />
           </div>
           <div className="author__details">
-           
-       
-        <div className="author__info">
-                <h6 className="author__name">Roaa Abushaqrah</h6>
-                <div className="author__social-media">
-              <img src={Facebook} alt="Social Media 1" className="author__social-icon" />
-              <img src={LinkeIn} alt="Social Media 2" className="author__social-icon" />
-              <img src={Tiwitter} alt="Social Media 3" className="author__social-icon" />
-              <img src={YouTube} alt="Social Media 4" className="author__social-icon" />
-            </div>
-            </div>
-              
-              
-              <span className="author__position">Software Engineering </span>
+          <div className="author__social-media">
+                <img src={Facebook} alt="Facebook" className="author__social-icon" />
+                <img src={LinkeIn} alt="LinkedIn" className="author__social-icon" />
+                <img src={Tiwitter} alt="Twitter" className="author__social-icon" />
+                <img src={YouTube} alt="YouTube" className="author__social-icon" />
+              </div>
+            <div className="author__info">
+              <h6 className="author__name">Roaa Abushaqrah</h6>
+              <span className="author__position">Software Engineering</span>
               <p className="author__bio">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi.
               </p>
+             
             </div>
           </div>
         </div>
-      
+      </div>
     </section>
   );
 }
--- src/pages/Blog/MarktingSide/About.jsx
+++ src/pages/Blog/MarktingSide/About.jsx
@@ -1,12 +1,36 @@
 import React from "react";
- 
-function About (){
-    return(
-       <section>
-        <div className="container">
-            
+import Roaa from "../../../assets/Roaa.jpg";  
+import '../../../styles/about.scss'
+
+function About() {
+  return (
+    <section className="about">
+      <div className="about__container">
+        <h6 className="about__title">About</h6>
+        <hr className="about__divider" />
+        <div className="about__content">
+          <div className="about__image-container">
+            <img src={Roaa} alt="Roaa Abu-sharah" className="about__image" />
+          </div>
+          <div className="about__info">
+            <h6 className="about__name">Roaa Abu-sharah</h6>
+            <span className="about__position">Software Engineer</span>
+          </div>
         </div>
-       </section>
-    )
+        <ul className="about__stats">
+          <li className="about__stat">
+            <img src="" alt="Date" className="about__stat-icon" /> October
+          </li>
+          <li className="about__stat">
+            <img src="" alt="Shared posts" className="about__stat-icon" /> 1K shared
+          </li>
+          <li className="about__stat">
+            <img src="" alt="Views" className="about__stat-icon" /> View
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 }
-export default About
+
+export default About;
