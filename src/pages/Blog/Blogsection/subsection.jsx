8ec93306e9b00bacd1b15b388fc7dc0531614f3c
roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-12-04 20:40:30 +0300

    testing

--- src/pages/Blog/Blogsection/subsection.jsx
+++ src/pages/Blog/Blogsection/subsection.jsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import Amzon from '../../../assets/image36.png';
-import '../../../styles/subblog.scss'
+import React from "react";
+import Amzon from "../../../assets/image36.png";
+import "../../../styles/subblog.scss";
 function SubSection() {
     return (
-       <>
-        <div className="blog__section">
-                    <h4 className="blog__heading">Lorem</h4>
-                    <p className="blog__text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia Feugiat et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia cursus morbi dui vulputate. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia cursus morbi dui vulputate. 
-                    </p>
-                    <figure className="blog__figure">
-  <img className="blog__image" src={Amzon} alt="Amazon" />
-  <figcaption className="blog__caption">
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia Feugiat et Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-  </figcaption>
-</figure>
-                </div>
+        <>
+            <div className="blog__section">
+                <h4 className="blog__heading">Lorem</h4>
+                <p className="blog__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia Feugiat et Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia cursus morbi dui vulputate. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Feugiat et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet.
+                    Lacinia cursus morbi dui vulputate.
+                </p>
+                <figure className="blog__figure">
+                    <img className="blog__image" src={Amzon} alt="Amazon" />
+                    <figcaption className="blog__caption">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia Feugiat et Lorem ipsum dolor sit
+                        amet, consectetur adipiscing elit.
+                    </figcaption>
+                </figure>
+                <hr />
+            </div>
 
-                <div className="blog__section">
-                    <h4 className="blog__heading">Ipsum</h4>
-                    <p className="blog__text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia Feugiat et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia cursus morbi dui vulputate. 
-                    </p>
-                    <p className="blog__texttwo">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia Feugiat et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia cursus morbi dui vulputate. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia cursus morbi dui vulputate. 
-                    </p>
-                </div>
-       </>
-
-            
-
-               
-
-          
-
-                
-           
+            <div className="blog__section">
+                <h4 className="blog__heading">Ipsum</h4>
+                <p className="blog__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia Feugiat et Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia cursus morbi dui vulputate.
+                </p>
+                <p className="blog__texttwo">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia Feugiat et Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet. Lacinia cursus morbi dui vulputate. Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit. Feugiat et Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat et feugiat auctor mauris nisi, pellentesque sagittis. In pulvinar quam ut varius ultrices id diam elit amet.
+                    Lacinia cursus morbi dui vulputate.
+                </p>
+            </div>
+        </>
     );
 }
 
