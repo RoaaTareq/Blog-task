16978260ff871ff7cbe5d855865ff0a3bb70bbad
roaaabushaqrah <roaa.abushaqrah@gmail.com>
2024-12-10 21:00:40 +0300

    blog

--- src/pages/Blog/MarktingSide/Subscrib.jsx
+++ src/pages/Blog/MarktingSide/Subscrib.jsx
@@ -1,8 +1,26 @@
 import React from "react";
- 
-function Subscribe (){
-    return(
-        <>jjj</>
-    )
+import "../../../styles/subscribe.scss";
+import Mail from "../../../assets/Mail-icon.svg";
+function Subscribe() {
+    return (
+        <section>
+            <div className="container">
+                <div className="subcribe">
+                    <div className="wrapp-title">
+                        <img src={Mail} alt="Mailimage" />
+                        <h1>
+                            Subscribe to our<b>Newsletter</b>
+                        </h1>
+                    </div>
+                    <p className="text-email">No spam, notifications only about new products, update</p>
+                    <div className="wrapp-email">
+               <input type="email" name="" id="" className="input-email" placeholder="Enter your Email" />
+               <button className="btn-submit">Submit</button>
+               </div>
+                </div>
+              
+            </div>
+        </section>
+    );
 }
-export default Subscribe
+export default Subscribe;
